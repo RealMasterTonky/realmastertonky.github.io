f8059a798b73423e78438d714bd9affbd312bfef
RealMasterTonky <121755215+RealMasterTonky@users.noreply.github.com>
2025-12-17 15:26:52 +0000

    Update main.js

--- main.js
+++ main.js
@@ -326,9 +326,9 @@ let pages = {
     },
     "https://realmastertonky.github.io/artworks": function() {
         document.body.innerHTML += "<h1>my artworks</h1>"
-        document.body.innerHTML += `<button style="font-size:30px" onclick="window.location.href='https://realmastertonky.github.io/site/artworks/atrocities'">atrocities</button><br>low quality caricatures that i drew for laughs and coughs<br><br>`
-        document.body.innerHTML += `<button style="font-size:30px" onclick="window.location.href='https://realmastertonky.github.io/site/artworks/digital'">digital drawings</button><br>pixels arranged in a way that makes (imo) decent images<br><br>`
-        document.body.innerHTML += `<button style="font-size:30px" onclick="window.location.href='https://realmastertonky.github.io/site/artworks/irl'">irl drawings</button><br>pictues of mmmm pencil scribbles with magik colored markers`
+        document.body.innerHTML += `<button style="font-size:30px" onclick="window.location.href='https://realmastertonky.github.io/artworks/atrocities'">atrocities</button><br>low quality caricatures that i drew for laughs and coughs<br><br>`
+        document.body.innerHTML += `<button style="font-size:30px" onclick="window.location.href='https://realmastertonky.github.io/artworks/digital'">digital drawings</button><br>pixels arranged in a way that makes (imo) decent images<br><br>`
+        document.body.innerHTML += `<button style="font-size:30px" onclick="window.location.href='https://realmastertonky.github.io/artworks/irl'">irl drawings</button><br>pictues of mmmm pencil scribbles with magik colored markers`
     },
     "https://realmastertonky.github.io/artworks/atrocities": function() {
         document.body.innerHTML += "<h1>atrocities</h1>"
@@ -456,6 +456,7 @@ function loadpage() {
 document.addEventListener("DOMContentLoaded", function() {
     loadpage()
 })
+
 
 
 
